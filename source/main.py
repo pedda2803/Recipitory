b44887216606e7a4b32023c2eaf0f72446f93216
pedda <jonas.p2803@gmail.com>
2025-01-15 20:49:54 +0100

    Move main.py to new location

--- source/main.py
+++ source/main.py
@@ -1,9 +1,11 @@
 from database.database_handler import *
+import tkinter
 
 DB = 'source/recipes.db'
 
 def main():
     database_handler.create_table()
+    tkinter._test()
     
 if __name__ == "__main__":
     database_handler = DatabaseHandler(DB)
